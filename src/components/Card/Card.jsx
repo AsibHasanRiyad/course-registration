467ce92861de4cd219c84ae0bfcbec02d14b2484
AsibHasanRiyad <asibhasanriyad@gmail.com>
2023-09-15 12:59:53 +0600

    all the card is added by using map and fake data

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,23 +1,32 @@
+import { useEffect, useState } from 'react';
 import { FaDollarSign, FaBookOpen } from 'react-icons/fa';
 
 const Card = () => {
+    const [courses , setCourses] = useState([]);
+    useEffect(() =>{
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => setCourses(data))
+    },[])
     return (
-        <div>
+        <div className=' flex '>
 
             {/* card section  div start*/}
-            <div>
-                    <div className=" p-4 rounded-xl w-[320px] bg-white">
+            <div className=' w-[80%] grid grid-cols-3 gap-6'>
+                    {
+                        courses.map(course =>
+                            <div key={course.id} className=" p-4 rounded-xl w-[320px] bg-white">
                     <figure className="">
-                        <img src="https://i.ibb.co/8gLhgVy/Rectangle-2-1.png" alt="Shoes" className="rounded-xl" />
+                        <img src={course.image} alt="Shoes" className="rounded-xl" />
                     </figure>
                     <div className=" items-center text-justify">
-                        <h2 className=" text-lg font-semibold text-center pt-4 pb-2">Introduction to C Programming</h2>
-                        <p className="text-[#1C1B1B99] font-normal ">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                        <h2 className=" text-lg font-semibold text-center pt-4 pb-2">{course.course_title} </h2>
+                        <p className="text-[#1C1B1B99] font-normal "> {course.details} </p>
                         {/* price and credit */}
                         <div className="flex justify-between mt-5 text-[#1C1B1B99] font-normal">
-                            <p className=' flex justify-center items-center'> <FaDollarSign /> <span className=' ml-2'>Price : 15000</span>
+                            <p className=' flex justify-center items-center'> <FaDollarSign /> <span className=' ml-2'>Price : {course.price} </span>
                             </p>
-                            <p className=' flex justify-center items-center'> <FaBookOpen /> <span className=' ml-2'>Credit : 1hr</span>
+                            <p className=' flex justify-center items-center'> <FaBookOpen /> <span className=' ml-2'>Credit : {course.credit}hr</span>
                             </p>
                         </div>
                         <div className="text-center">
@@ -25,8 +34,14 @@ const Card = () => {
                         </div>
                     </div>
                     </div>
+                            )
+                    }
             </div>
             {/* card div end */}
+
+            <div className=' w-[20%]'>
+                <h1 className='text-5xl'>Cart</h1>
+            </div>
 
         </div>
     );
