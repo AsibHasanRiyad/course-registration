9da0179124dcd82299751a07cbf6d3b7080e729f
AsibHasanRiyad <asibhasanriyad@gmail.com>
2023-09-15 11:52:50 +0600

    all set up done and tailwind,daisy ui, font inter is added

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Hello World.</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio officia, deleniti facilis error eius assumenda a. Dignissimos maiores perferendis sed excepturi pariatur consequatur, iste eius illo tenetur mollitia, laudantium maxime molestias atque ullam tempore perspiciatis consequuntur, accusamus dolore eos accusantium. Blanditiis eveniet corporis totam magni voluptatibus quidem eaque autem consectetur et animi quos accusantium facere dolorem a libero, praesentium quas nam velit quasi maiores sit? Vel dicta voluptatibus minima blanditiis non provident molestiae perspiciatis, voluptates eveniet recusandae, odio consequatur, quo natus eligendi tenetur dolorem accusantium ad quidem dolorum maiores ab! Quis incidunt molestiae, laboriosam esse odio necessitatibus maiores alias tempore?</p>
+        </div>
+    );
+};
+
+export default Home;
